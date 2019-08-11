44983f14674ff822b978b825143a53840743cc4c
anderjs <andersonav37@gmail.com>
2019-08-10 20:33:58 -0400

    typeofall.js

---
+++ src/functions/typeofall.js
@@ -0,0 +1,18 @@
+
+/**
+ * @description
+ * You can have a number of arguments of the same type where 
+ * you make a typeof for each argument of your function, when you use typeof all,
+ * you can directly tell if all these arguments are of the same type.
+ * @param {'string' | 'number' | 'object' | 'undefined' | 'null' | 'Symbol' | 'function'} type 
+ * @param  {...any} args N based arguments.
+ * @param {boolean} inverseMode Negates the statement, typeof instantly goes !==
+ * @returns {boolean} 
+ * @example
+ * typeofAll('number', 1, 2, 3, 4, 5) => true
+ */
+function typeofAll(type, inverseMode, ...args) {
+  return inverseMode ? args.every(arg => typeof arg !== type) : args.every(arg => typeof arg === type)
+}
+
+module.exports = typeofAll
